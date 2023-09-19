f95999f1ee56abdf8cf799224a9eb0e9d1d2a22b
timre13 <itorteli13@gmail.com>
2023-09-19 11:44:01 +0200

    Add slider

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
 import "./style.scss";
+
+import Sliderm from "sliderm";
+import "sliderm/src/assets/scss/index.scss";
+
+const sliderm = new Sliderm('#main-slider', {
+    arrow: true,
+    pagination: true,
+    grouping: false,
+    loop: true,
+    preview: false,
+    columns: 1,
+    duration: 300,
+    spacing: 10,
+    autoplay: true,
+    align: 'center',
+});
